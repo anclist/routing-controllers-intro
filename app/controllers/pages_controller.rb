6b6528d235d17c552fb783a7f60d30b52c5606d3
Tony <ANCLIST@GMAIL.COM>
2017-10-11 15:38:57 -0400

    Define a dynamic route that uses the kittens controller to show cats images of diferent sizes usin the lorem pixel image site

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,27 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
 
-def welcome
-  @header = "Welcome to our site"
-end
+  def welcome
+    @header = "Welcome to our site"
+  end
 
-def about
-  @header = "About Us"
-end
+  def about
+    @header = "About Us"
+  end
 
-def contest
-  @header = "Get ready to win!!"
-end
+  def contest
+    @header = "Get ready to win!!"
+  end
 
-def kitten
-  requested_size = params[:size]
-  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
-end
+  def kitten
+  end
+
+  def kittens
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ get '/welcome' => 'pages#welcome'
 get '/about' => 'pages#about'
 get '/contest' => 'pages#contest'
 get '/kitten/:size' => 'pages#kitten'
+get '/kittens/:size' => 'pages#kittens'
 end
