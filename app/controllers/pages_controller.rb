d0e353b81b113191495be6caa636956dfeea6bb8
Tony <ANCLIST@GMAIL.COM>
2017-10-11 15:15:59 -0400

    Define an instance variable named header for each route, that matches the variable on the layout view, to have an h1 header for each page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
 
 def welcome
+  @header = "Welcome to our site"
 end
 
 def about
+  @header = "About Us"
 end
 
 def contest
+  @header = "Get ready to win!!"
 end
 
 end
